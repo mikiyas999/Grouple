4c46a6d28461c802b41fdcf3a5f9ab9083ba29a5
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-09-05 14:50:03 +0100

    implement clerk sign-in and sign-up wit custom components

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,32 @@
+import SignInForm from "@/components/forms/sign-in"
+import { GoogleAuthButton } from "@/components/global/google-oauth-button"
+import { Separator } from "@/components/ui/separator"
+
+type Props = {}
+
+const page = (props: Props) => {
+    return (
+        <>
+            <div className="flex flex-col space-y-2">
+                <h5 className="font-bold text-base text-themeTextWhite">
+                    Login
+                </h5>
+                <p className="text-themeTextGray leading-tight">
+                    Network with people from around the world, join groups,
+                    create your own, watch courses and become the best version
+                    of yourself.
+                </p>
+            </div>
+            <SignInForm />
+            <div className="my-10 w-full relative">
+                <div className="bg-black p-3 absolute text-themeTextGray text-xs top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    OR CONTINUE WITH
+                </div>
+                <Separator orientation="horizontal" className="bg-themeGray" />
+            </div>
+            <GoogleAuthButton method="signin" />
+        </>
+    )
+}
+
+export default page
---
+++ src/app/group/create/page.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+const page = (props: Props) => {
+    return <div>Group create page</div>
+}
+
+export default page
