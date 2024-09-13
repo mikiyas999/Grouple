2b0da62130b5f687f0e0daaaca5938e09686ca0b
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-09-13 18:26:20 +0100

    add truncateString util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export const truncateString = (string: string) => {
+    return string.slice(0, 60) + "..."
+}
