40c364cb61b55882291c99a692d4102223e01433
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-09-20 18:58:36 +0100

    course content form

--- src/components/forms/course-content/index.tsx
+++ src/components/forms/course-content/index.tsx
@@ -6,61 +6,65 @@ import { Button } from "@/components/ui/button"
 import { useCourseContent, useCourseSectionInfo } from "@/hooks/courses"
 
 type CourseContentFormProps = {
-  sectionid: string
-  userid: string
-  groupid: string
+    sectionid: string
+    userid: string
+    groupid: string
 }
 
 export const CourseContentForm = ({
-  sectionid,
-  userid,
-  groupid,
-}: CourseContentFormProps) => {
-  const { data } = useCourseSectionInfo(sectionid)
-
-  const {
-    errors,
-    onUpdateContent,
-    setJsonDescription,
-    setOnDescription,
-    onEditDescription,
-    setOnHtmlDescription,
-    editor,
-    isPending,
-  } = useCourseContent(
     sectionid,
-    data?.section?.content || null,
-    data?.section?.JsonContent || null,
-    data?.section?.htmlContent || null,
-  )
+    userid,
+    groupid,
+}: CourseContentFormProps) => {
+    const { data } = useCourseSectionInfo(sectionid)
 
-  return groupid === userid ? (
-    <form onSubmit={onUpdateContent} className="p-5 flex flex-col" ref={editor}>
-      <BlockTextEditor
-        onEdit={onEditDescription}
-        max={10000}
-        inline
-        min={100}
-        disabled={userid === groupid ? false : true}
-        name="jsoncontent"
-        errors={errors}
-        setContent={setJsonDescription || undefined}
-        content={JSON.parse(data?.section?.JsonContent!)}
-        htmlContent={data?.section?.htmlContent || undefined}
-        setHtmlContent={setOnHtmlDescription}
-        textContent={data?.section?.content || undefined}
-        setTextContent={setOnDescription}
-      />
-      {onEditDescription && (
-        <Button
-          className="mt-10 self-end bg-themeBlack border-themeGray"
-          variant="outline"
+    const {
+        errors,
+        onUpdateContent,
+        setJsonDescription,
+        setOnDescription,
+        onEditDescription,
+        setOnHtmlDescription,
+        editor,
+        isPending,
+    } = useCourseContent(
+        sectionid,
+        data?.section?.content || null,
+        data?.section?.JsonContent || null,
+        data?.section?.htmlContent || null,
+    )
+
+    return groupid === userid ? (
+        <form
+            onSubmit={onUpdateContent}
+            className="p-5 flex flex-col"
+            ref={editor}
         >
-          <Loader loading={isPending}>Save Content</Loader>
-        </Button>
-      )}
-    </form>
-  ) : (
-    <HtmlParser html={data?.section?.htmlContent!} />
-  )
+            <BlockTextEditor
+                onEdit={onEditDescription}
+                max={10000}
+                inline
+                min={100}
+                disabled={userid === groupid ? false : true}
+                name="jsoncontent"
+                errors={errors}
+                setContent={setJsonDescription || undefined}
+                content={JSON.parse(data?.section?.JsonContent!)}
+                htmlContent={data?.section?.htmlContent || undefined}
+                setHtmlContent={setOnHtmlDescription}
+                textContent={data?.section?.content || undefined}
+                setTextContent={setOnDescription}
+            />
+            {onEditDescription && (
+                <Button
+                    className="mt-10 self-end bg-themeBlack border-themeGray"
+                    variant="outline"
+                >
+                    <Loader loading={isPending}>Save Content</Loader>
+                </Button>
+            )}
+        </form>
+    ) : (
+        <HtmlParser html={data?.section?.htmlContent!} />
+    )
 }
