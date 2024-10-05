fae9ee8f2c782c7421ac5686acfa1acdb3aeefae
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-10-05 15:14:02 +0100

    final commit

---
+++ src/app/group/[groupId]/settings/domains/page.tsx
@@ -0,0 +1,11 @@
+type Props = { params: { groupid: string } }
+
+const DomainConfigPage = async ({ params }: Props) => {
+    return (
+        <div className="flex items-center justify-center">
+            <h3>Coming soon</h3>
+        </div>
+    )
+}
+
+export default DomainConfigPage
